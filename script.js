2a79cffb61a171e269df7f7c0adb86518a2c0a23
Lucas Carvalho <104842709+lucasdmmc@users.noreply.github.com>
2022-08-26 14:11:16 -0300

    Ajustes finais da logica para o menu

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const buttonOpen = document.querySelector(".open-menu")
 const buttonClose = document.querySelector(".close-menu")
 const links = document.querySelector(".container-links")
+const main = document.querySelector("main")
 
 buttonOpen.addEventListener("click", () => {
   buttonOpen.classList.add('hide')
   buttonClose.classList.remove("hide")
   links.classList.remove("hide")
+  main.classList.add("hide")
 
 })
 
@@ -13,4 +15,5 @@ buttonClose.addEventListener("click", () => {
   buttonClose.classList.add("hide")
   buttonOpen.classList.remove("hide")
   links.classList.add("hide")
+  main.classList.remove("hide")
 })
